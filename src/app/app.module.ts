31d4b75a8c906f39b4bd2b4e6c886eec76c94e06
Jesus CZ <jesus.za@yespointos.com>
2020-12-02 14:28:33 -0600

    first view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing, appRoutingProviders} from './app.routing';
 import { AppComponent } from './app.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContainerComponent } from './components/container/container.component';
+import { SearchComponent } from './components/search/search.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoadOneComponent } from './components/load-one/load-one.component';
+import { LoadVariousComponent } from './components/load-various/load-various.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    HeaderComponent,
+    ContainerComponent,
+    SearchComponent,
+    GalleryComponent,
+    UsuarioComponent,
+    LoginComponent,
+    RegistroComponent,
+    LoadOneComponent,
+    LoadVariousComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
